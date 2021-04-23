9cc1c9f8e6fab620d16be1b7503235bdfc9c8278
Jacob <hillj@stpaulsschool.org.uk>
2021-04-23 12:10:21 +0100

    Add project files.

---
+++ Vectors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2Dint v1 = new Vector2Dint(1, 2);
+            Vector2Dint v2 = new Vector2Dint(3, 5);
+            v1.AddTo(v2).PrintToConsole();
+            v1.ScalarMultiply(5).PrintToConsole();
+            Console.WriteLine(v1.DotProduct(v2));
+            Console.Read();
+        }
+    }
+}
---
+++ Vectors/Vector2Dint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vectors
+{
+    public class Vector2Dint
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public Vector2Dint(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+        public void PrintToConsole()
+        {
+            Console.WriteLine("(" + x.ToString() + ", " + y.ToString() + ")");
+        }
+        public Vector2Dint AddTo(Vector2Dint v)
+        {
+            return new Vector2Dint(x + v.x, y + v.y);
+        }
+        public Vector2Dint ScalarMultiply(int i)
+        {
+            return new Vector2Dint(i*x, i*y);
+        }
+        public int DotProduct(Vector2Dint v)
+        {
+            return x * v.x + y * v.y;
+        }
+
+    }
+}
